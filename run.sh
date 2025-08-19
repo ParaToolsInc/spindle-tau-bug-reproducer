067cda33e74dbae394b52ac644ce0268e005e735
Nicholas Chaimov <nchaimov@paratools.com>
2025-08-19 09:48:34 -0700

    Show failure occurs with `script` but not `perl script`

--- run.sh
+++ run.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-export PATH="$PWD:$PWD/scripts:$PATH"
 
+printf "Invoke script as argument to perl:\n"
+perl ./same-dir-script
+perl ./scripts/other-dir-script
+
+printf "\nInvoke script as executable:\n"
+export PATH="$PWD:$PWD/scripts:$PATH"
 same-dir-script
 other-dir-script
 
